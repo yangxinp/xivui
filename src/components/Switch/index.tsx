a0382f495924ba4918edc943c342c9d16b424f64
Simon <naclyang@foxmail.com>
2023-11-28 19:23:22 +0800

    feat(Switch): prop loading, disabled

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -1,7 +1,10 @@
 import { defineComponent, h, resolveDirective, withDirectives } from "vue";
 import { Ripple } from "../../directives/ripple"
+import ProgressCircular from "../ProgressCircular";
 
 const Switch = defineComponent({
+  emits: ['click', 'input', 'update:value'],
+
   name: 'x-switch',
   directives: {
     Ripple
@@ -9,11 +12,13 @@ const Switch = defineComponent({
   props: {
     inset: Boolean,
     value: Boolean,
-    onClick: Function,
+    loading: Boolean,
+    disabled: Boolean,
   },
   methods: {
     handleClick () {
       const value = !this.value
+      this.$emit('click')
       this.$emit('input', value)
       this.$emit('update:value', value)
     },
@@ -21,7 +26,11 @@ const Switch = defineComponent({
       return h('div', { class: "x-switch--track" })
     },
     _renderThumb () {
-      return h('div', { class: "x-switch--thumb" })
+      const child = this.loading ?
+        h(ProgressCircular, { indeterminate: true  }) :
+        undefined
+
+      return h('div', { class: "x-switch--thumb" }, child)
     },
     _renderRipple () {
       const VRipple = resolveDirective('ripple')
@@ -35,6 +44,8 @@ const Switch = defineComponent({
     return h('div', {
       class: {
         "x-switch": true,
+        "loading": this.loading,
+        "disabled": this.disabled,
         "active": this.value,
         "inset": this.inset
       },
