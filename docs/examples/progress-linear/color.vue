1d2ecd47eb7e68c3ee4aca540ddfac1467e8a5fd
Simon <naclyang@foxmail.com>
2023-11-28 19:21:39 +0800

    docs(ProgressLinear): improve docs

---
+++ docs/examples/progress-linear/color.vue
@@ -0,0 +1,9 @@
+<template>
+  <x-progress-linear class="text-blue" :value="20" />
+  <br />
+  <x-progress-linear class="text-red" :value="30" :buffer-value="80" />
+  <br />
+  <x-progress-linear class="text-green" :value="20" :buffer-value="40" stream />
+  <br />
+  <x-progress-linear class="text-brown" indeterminate stream />
+</template>
---
+++ docs/examples/progress-linear/height.vue
@@ -0,0 +1,8 @@
+<template>
+  <x-progress-linear :value="30" height="2px" />
+  <br />
+  <x-progress-linear :value="30" />
+  <br />
+  <x-progress-linear :value="30" :height="6" />
+  <br />
+</template>
