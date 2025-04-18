96fe2584fd4c89d8da19427f842ee028ac4b2a26
Simon <naclyang@foxmail.com>
2025-04-18 20:46:39 +0800

    chore: enhance the deployment procedures

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 username="root"
-domain="124.223.57.10"
+domain="0.0.0.0"
 port="22"
 dist="./docs/.vitepress/dist/*"
 web_root="/usr/share/nginx/xivui/"
+
+sudo mkdir -p $web_root
+
+ssh $username@$domain -t "mkdir $web_root"
 
 scp -r -P $port $dist $username@$domain:$web_root
 
